dccd6dfc23f1720ba70cc4149aed35d1569f8280
fkhjoy <khjoyfoysal@gmail.com>
2021-10-26 08:58:34 +0600

    added difficulty

--- quiz_project/student/urls.py
+++ quiz_project/student/urls.py
@@ -11,7 +11,8 @@ urlpatterns = [
     
     path('student-exam', views.student_exam_view, name='student-exam'),
     path('take-exam/<int:pk>', views.take_exam_view, name='take-exam'),
-    path('start-exam/<int:pk>', views.start_exam_view, name='start-exam'),
+    # path('start-exam/<int:pk>/', views.start_exam_view, name='start-exam'),
+    path('start-exam/<int:pk>/<str:lv>/',views.start_exam_view, name='start-exam'),
 
     path('calculate-marks', views.calculate_marks_view, name='calculate-marks'),
     path('view-result', views.view_result_view, name='view-result'),
--- quiz_project/student/views.py
+++ quiz_project/student/views.py
@@ -67,6 +67,7 @@ def student_exam_view(request):
 @login_required(login_url='studentlogin')
 @user_passes_test(is_student)
 def take_exam_view(request, pk):
+    
     course = QMODEL.Course.objects.get(id=pk)
     total_questions = QMODEL.Question.objects.all().filter(course=course).count()
     questions = QMODEL.Question.objects.all().filter(course=course)
@@ -80,13 +81,15 @@ def take_exam_view(request, pk):
 @login_required(login_url='studentlogin')
 @user_passes_test(is_student)
 @csrf_exempt
-def start_exam_view(request, pk, lk):
+def start_exam_view(request, pk, lv=None):
     course = QMODEL.Course.objects.get(id=pk)
+    level = lv
+    print(lv)
     questions = QMODEL.Question.objects.all().filter(course=course)
     if request.method == 'POST':
         pass
     response = render(request, 'student/start_exam.html',
-                      {'course': course, 'questions': questions})
+                      {'course': course, 'questions': questions, 'level': level})
     response.set_cookie('course_id', course.id)
     return response
 
